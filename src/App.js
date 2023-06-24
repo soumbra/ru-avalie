cb7d8995d6574282c91dc61c37b4b2d32d29e4e5
Franciel Silveira <francielsilveiraredes@gmail.com>
2023-06-24 20:12:40 -0300

    Organização total dos arquivos
    Projeto organizado completamente

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainPage from './components/RuAvalie/MainPage';
+import MyMenu2 from './components/RuAvalie/Menuv2';
+//import Login from './components/RuAvalie/Login';
+//import MyMenu from './components/RuAvalie/Menu';
+
+function App() {
+  return (
+      <div className="App">
+        <MainPage />
+      </div>
+  );
+}
+
+export default App;
+
