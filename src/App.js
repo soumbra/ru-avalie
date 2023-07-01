1fd12856364d36c586e8d2f2312f6c636c447372
soareswt <victorsuares2014@gmail.com>
2023-07-01 17:21:31 -0300

    update project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
-import MainPage from './components/RuAvalie/MainPage';
-import MyMenu2 from './components/RuAvalie/Menuv2';
-//import Login from './components/RuAvalie/Login';
+import "./App.css";
+import MainPage from "./components/MainPage";
+//import MyMenu2 from "./components/RuAvalie/Menuv2";
+import Login from "./components/Login";
 //import MyMenu from './components/RuAvalie/Menu';
 
 function App() {
   return (
-      <div className="App">
-        <MainPage />
-      </div>
+    <div className="App">
+      <MainPage />
+    </div>
   );
 }
 
 export default App;
-
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,25 +1,34 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import MyMenu2 from "./Menuv2"
-import { Container, Typography } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyMenu2 from "./Menuv2";
+import { Container, Typography } from "@mui/material";
 //Rotas
-import Votar from "./Votar"
-import Resultados from "./Resultados"
+import Votar from "./Votar";
+import Resultados from "./Resultados";
+import Login from "./Login";
 
 //Setando as rotas do projeto (install react-router-dom)
 const MainPage = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <MyMenu2 />
-                    <Container sx={{mt:8}}> 
-                        <Routes>
-                            <Route path="votarRuAvalie" element={<Votar data="Almoço 12/05/2024" />}/>
-                            <Route path="resultadosRuAvalie" element={<Resultados data="12/04/2021" />} />
-                        </Routes>
-                    </Container>
-            </BrowserRouter>
-        </>
-    )
-}
+  return (
+    <>
+      <BrowserRouter>
+        <MyMenu2 />
+        <Container sx={{ mt: 8 }}>
+          <Routes>
+            <Route path="/" element={<Login />}></Route>
+            <Route path="*" element={<Login />}></Route>
+            <Route
+              path="/votarRuAvalie"
+              element={<Votar data="Almoço 12/05/2024" />}
+            />
+            <Route
+              path="/resultadosRuAvalie"
+              element={<Resultados data="12/04/2021" />}
+            />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default MainPage
+export default MainPage;
